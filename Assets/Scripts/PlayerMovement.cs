75dbdd3cd9813b566cf0df1c2b028f22c9e879f1
ErikAndersson1 <erikandersson.kvarntorp@hotmail.com>
2024-01-31 15:26:07 +0100

    cleanup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,6 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movement;
 
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
