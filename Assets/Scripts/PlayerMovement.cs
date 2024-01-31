c4cf372b32c3c49b8e4d5f402b7e2fcbf7c0126b
ErikAndersson1 <erikandersson.kvarntorp@hotmail.com>
2024-01-31 15:22:56 +0100

    Player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = Vector2.ClampMagnitude(movement, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (movement * speed * Time.fixedDeltaTime));
+    }
+}
