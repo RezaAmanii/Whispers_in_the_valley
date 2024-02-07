ce3c8313b04436f3fbea9be2afce446bb3932225
Reza Amani <113048496+RezaAmanii@users.noreply.github.com>
2024-02-07 15:26:44 +0100

    Everything woks
    Have copied all the script from the earlier version of my branch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,55 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 100f;
     public Rigidbody2D rb;
-    private Vector3 movementDirection;
+    private Vector2 movementDirection;
     public float x, y;
     public Animator animator;
     private bool isWalking;
 
 
-    private void Awake()
+    public void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        // Running 
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            speed = 5f;
-        else
-            speed = 2f;
+
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        if(x != 0 || y != 0)
+        {
+            animator.SetFloat("Horizontal", x);
+            animator.SetFloat("Vertical", y);
+
+            if (!isWalking)
+            {
+                isWalking = true;
+                animator.SetBool("isMoving", isWalking);
+            }
+
+        } else
+        {
+            if (isWalking)
+            {
+                isWalking = false;
+                animator.SetBool("isMoving", isWalking);
+                StopMoving();
+            }
+        }
 
 
         movementDirection = new Vector3(x, y).normalized;
 
-       
+        // Running 
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            speed = 180f;
+        else
+            speed = 100f;
+
     }
 
     private void StopMoving()
     {
-        rb.velocity = Vector3.zero;
+        rb.velocity = Vector2.zero;
     }
 
     private void FixedUpdate()
     {
         rb.velocity = movementDirection * speed * Time.deltaTime;
 
-        // Running
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            speed = 180f;
-        else
-        {
-            speed = 100f;
-        }
 
     }
 }
