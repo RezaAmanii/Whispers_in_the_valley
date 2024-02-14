9d7b4303eb1e165e7a9005822282e6086d761bca
Reza Amani <113048496+RezaAmanii@users.noreply.github.com>
2024-02-14 11:58:51 +0100

    Checkpoints, and a popup text that shows the game is saved

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckPoint : MonoBehaviour
 {
 
     private GameMaster gameMaster;
+    public GameObject popupTextPrefab;
+    public TMP_Text popupText;
 
     private void Start()
     {
@@ -16,6 +19,8 @@ public class CheckPoint : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            
+            Instantiate(popupTextPrefab, transform.position, Quaternion.identity);
             gameMaster.lastCheckPointPosition = transform.position;
         }
         
