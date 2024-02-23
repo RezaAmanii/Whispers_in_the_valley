cdd6379f1ac3714cfc23c6a9969752257269a01a
OxyRust <97917116+OxyRust@users.noreply.github.com>
2024-02-23 11:44:33 +0100

    Changes

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -10,9 +10,36 @@ public class SceneHandler : MonoBehaviour
     private GameObject MC;
 
 
-    private string[] Scenes = { "TheWhisperingInn", "TownScene", "Townscene", "TownScene", "TownScene", "TownScene", "Townscene", "TownScene", "TownScene", "ChurchScene" }; //TownScene exits in order = 
-    // WhisperingInn(1), Teresa (2), Paul (3), Nadine (4), Matthew (5), Alex (6), Julia (7), Church (8)
-    private Vector2[] positions = { new Vector2(-9,-20), new Vector2(-12, -12), new Vector2(-6, 1), new Vector2(-1, -17), new Vector2(-15, -4), new Vector2(13, -16), new Vector2(10, 1), new Vector2(24, -6), new Vector2(0, -4) };
+    private string[] Scenes = { 
+        "TheWhisperingInn", // WhisperingInn(0)
+        "TownScene",        // Town @ WhisperingInn(1)
+        "Townscene",        // Town @ Alex(2)
+        "TownScene",        // Town @ Paul(3)
+        "TownScene",        // Town @ Nadine(4)
+        "TownScene",        // Town @ Mathew(5)
+        "Townscene",        // Town @ Alex(6)?
+        "TownScene",        // Town @ Julia(7)
+        "TownScene",        // Town @ Church(8)
+        "ChurchScene",      // Church(9)
+        "AlexsHouseScene",  // AlexsHouse(10)
+        "JuliasHouseScene", // JuliasHouse(11)
+        "MathewsHouseScene", // MathewsHouse(12)
+        "PaulsHouseScene", // PaulsHouse(13)
+        "NadinesHouseScene" // NadinesHouse(14)
+        }; 
+       
+    private Vector2[] positions = { 
+        new Vector2(-9,-20),    // WhisperingInn(0)
+        new Vector2(-12, -12),  // Town @ WhisperingInn(1)
+        new Vector2(-6, 1),     // Town @ Alex(2)
+        new Vector2(-1, -17),   // Town @ Paul(3)
+        new Vector2(-15, -4),   // Town @ Nadine(4)
+        new Vector2(13, -16),   // Town @ Mathew(5)
+        new Vector2(10, 1),     // Town @ Alex(6)?
+        new Vector2(24, -6),    // Town @ Julia(7)
+        new Vector2(0, -4),     // Town @ Church(8)
+        
+         };
 
 
     // Start is called before the first frame update
